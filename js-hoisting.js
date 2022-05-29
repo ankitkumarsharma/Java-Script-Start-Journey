ac42d7d5577dd6d8d8b4f7fd8eb78fda1ab3e7a4
ankitkumarsharma <aksharma.dixit@gmail.com>
2022-05-29 23:36:50 +0530

    added js hoisting part

--- js-hoisting.js
+++ js-hoisting.js
@@ -1,14 +1,22 @@
-xyz(); // function hoisting
-
-console.log(x) // js variable hoisting
-console.log(xyz) // function hoisting
-
+// variable hoisting
+console.log(x) // output - undefined
+y = 3;
+console.log(y) // output - 3
 var x = 7;
-
+var y;
+// function hoisting
+xyz();  // output - xyz is calling...
+console.log(xyz)  // output - function defined
 function xyz(){
     console.log("xyz is calling...");
 }
-console.log(xyz)
+console.log(xyz);
+// variable defined arroe function hoisting
+console.log(arrowFn) // output - undefined
+var arrowFn = ()=>{
+    console.log("arrowFn is calling ...")
+}
+console.log(arrowFn) // output - function defined
 
 // js hoisting, it create global space as execution context on call stack
 // In JavaScript, a variable can be declared after it has been used.
