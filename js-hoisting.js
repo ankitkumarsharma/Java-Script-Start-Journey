c8fdff9edb4b114f1fbf865e044aa36c82d24b61
ankitkumarsharma <aksharma.dixit@gmail.com>
2022-07-10 02:48:28 +0530

    add js hoisting points

--- js-hoisting.js
+++ js-hoisting.js
@@ -1,22 +1,27 @@
-// variable hoisting
+//--- variable hoisting
+
 console.log(x) // output - undefined
 y = 3;
 console.log(y) // output - 3
 var x = 7;
 var y;
-// function hoisting
+
+//--- function hoisting
+
 xyz();  // output - xyz is calling...
-console.log(xyz)  // output - function defined
+console.log(xyz)  // output - function xyz(){...} defined
 function xyz(){
     console.log("xyz is calling...");
 }
-console.log(xyz);
-// variable defined arroe function hoisting
+console.log(xyz); // output - function xyz(){...} defined
+
+//--- variable defined arrow function hoisting
+
 console.log(arrowFn) // output - undefined
 var arrowFn = ()=>{
     console.log("arrowFn is calling ...")
 }
-console.log(arrowFn) // output - function defined
+console.log(arrowFn) // output - function ()=>{...} defined
 
 // js hoisting, it create global space as execution context on call stack
 // In JavaScript, a variable can be declared after it has been used.
